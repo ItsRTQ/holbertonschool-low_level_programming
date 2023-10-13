718fe6518a4468fca98686e34c2151aa5e522068
root <titanbabrisa@gmail.com>
2023-10-12 19:39:43 -0700

    Commit msg

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Is the main function, it starts the program
+ *
+ * Description - This function will print the desire string using printf
+ * Return: the return here, returns 0 thats basically void or nothing
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
