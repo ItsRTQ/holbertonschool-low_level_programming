e8563ac99f78727ab699724075e7d94b9f5f5274
root <titanbabrisa@gmail.com>
2023-10-12 19:26:09 -0700

    Commit msg

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - This is the main function of the program
+*
+* Description - This function will print, Hello, World!, with the function puts
+* Return: The return doesn't return anything, thats what zero means
+*/
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
