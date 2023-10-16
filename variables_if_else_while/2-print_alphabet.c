a105d7167614819836dbf8ba44495da3eb3b0330
root <titanbabrisa@gmail.com>
2023-10-16 08:32:30 -0700

    first attempt

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - starts the program
+ * Description - prints the alphabet
+ * Return: 0 is the fuction finish correctly
+ */
+int main(void)
+{
+	char alphabet;
+	alphabet = "abcdefghijklmnopqrstuvwxyz";
+	putchar(ch);
+	return (0);
+}
