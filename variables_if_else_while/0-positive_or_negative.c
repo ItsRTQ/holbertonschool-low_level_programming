389a6149c6102a717261c3758bcfc5a2c9db0ba6
root <titanbabrisa@gmail.com>
2023-10-16 07:24:36 -0700

    first summit

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - main function starts the code
+ *
+ * Description - the main funciton check is the number entered is a positivive or negative
+ * Return: Thhe fuction return 0 when it finish, letting know it has finish
+ */
+int main(void)
+{
+	if(n >= 0)
+	{
+		if(n == 0)
+		{
+			puts("%d is zero", n)
+		}
+		else
+		{
+			printf("%d is positive", n)
+		}
+	}
+	else
+	{
+		printf("%d is negative", n);
+	}
+	return (0);
+}
