7360e49009d6c5270b235c12dce4be7830891dd1
root <titanbabrisa@gmail.com>
2023-10-16 11:57:17 -0700

    first attempt

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - starts program
+ * Description - prints numbers from 0 to 9
+ * Return: 0 if the function finish correctly
+ */
+int main(void)
+{
+	int a;
+	for (a = 0; a < 10; a++)
+	{
+		printf("%u", a);
+	}
+	printf("\n");
+	return (0);
+}
