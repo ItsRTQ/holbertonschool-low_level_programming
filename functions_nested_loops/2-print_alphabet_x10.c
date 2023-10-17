2293e1f8a36222c7aaea4ad296b43e479d32380a
root <titanbabrisa@gmail.com>
2023-10-17 06:20:58 -0700

    first attempt

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet_x10 - prints de abc's
+ * Description - prints the abc's 10 times
+ * Return: doesn't return, it return is void
+ */
+void print_alphabet_x10(void)
+{
+	int ch;
+	int rep;
+
+	for (rep = 0; rep < 10; rep++)
+	{
+		for (ch = 97; ch <= 122; ch++)
+		{
+			_putchar(ch);
+		}
+		putchar('\n');
+	}
+}
