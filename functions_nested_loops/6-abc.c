e7bc7b80d81378744c372328dd29d06f2a5eba2d
William <titanbabrisa@gmail.com>
2023-10-17 12:03:51 -0400

    save

---
+++ functions_nested_loops/6-abc.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _abs - starts the program
+ * @n: recive the int value use for the function
+ * Description - computes the absolute value of an integer
+ * Return: the result of all the computes
+ */
+int _abs(int n);
+{
+}
