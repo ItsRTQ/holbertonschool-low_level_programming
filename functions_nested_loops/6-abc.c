81e8cc7b5c3905eaf395bcd2efb4ec2b6ba8c5f5
William <titanbabrisa@gmail.com>
2023-10-17 13:36:48 -0400

    first attempt

--- functions_nested_loops/6-abc.c
+++ functions_nested_loops/6-abc.c
@@ -5,6 +5,14 @@
  * Description - computes the absolute value of an integer
  * Return: the result of all the computes
  */
-int _abs(int n);
+int _abs(int n)
 {
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
 }
