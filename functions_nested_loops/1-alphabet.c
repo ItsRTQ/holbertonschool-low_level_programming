c0ad3dca094edfd4a664c79066568fba402475f8
root <titanbabrisa@gmail.com>
2023-10-16 19:14:13 -0700

    first attempt

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * print_alphabet - prints the abc's
+ * Description - prints the alphabet using _putchar
+ * Return: is void it doesn't return
+ */
+void print_alphabet(void);
+{
+	int ch;
+
+	for (ch = 97; ch <= 122; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
