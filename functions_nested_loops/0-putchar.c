980816b4427014877017f6a7ca3ec8e06267def5
root <titanbabrisa@gmail.com>
2023-10-16 18:27:26 -0700

    first attempt

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - starts the program
+ * Description - prints _putchar and returns 0
+ * Return: 0 if the function finish propertly
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
