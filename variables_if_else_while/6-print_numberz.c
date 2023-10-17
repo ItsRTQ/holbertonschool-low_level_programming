8d0307ddb6f95f4d103e6d64e35191fab97aade7
root <titanbabrisa@gmail.com>
2023-10-16 17:01:53 -0700

    first attempt

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - starts the program
+ * Description - counts from 0 to 9
+ * Return: 0 if the fuction finished correctly
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 0;ch < 10; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
