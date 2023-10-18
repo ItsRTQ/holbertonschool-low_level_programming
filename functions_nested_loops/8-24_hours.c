09b712f5ca96f6d5adb265a0645fb3379557e84d
William <titanbabrisa@gmail.com>
2023-10-17 21:19:11 -0400

    My Commit

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * jack_bauer - starts the program
+ * Description - prints all the hours of the day starting from 00:00
+ * Return: is void, doesnt return anything
+ */
+void jack_bauer(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+
+	for (a = 0; a <= 2; a++)
+	{
+		for (b = 0; b <= 3; b++)
+		{
+			for (c = 0; c <= 5; c++)
+			{
+				for (d = 0; d <= 9; d++)
+				{
+					_putchar('0' + a);
+					_putchar('0' + b);
+					_putchar(58);
+					_putchar('0' + c);
+					_putchar('0' + d);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
