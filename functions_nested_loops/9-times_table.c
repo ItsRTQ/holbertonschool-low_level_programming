4f3cd8405d2c26108fbe434ef4fb2a8517d1dada
William <titanbabrisa@gmail.com>
2023-10-18 11:53:35 -0400

    first attempt

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * times_table - starts the function
+ * Description - print the times tables fo 0 to 9
+ * Return: is void, doenst return
+ */
+void times_table(void)
+{
+	int total;
+	int tabla;
+	int por;
+	int firstInt;
+
+	for (por = 0; por <= 9; por++)
+	{
+		for (tabla = 0; tabla <= 9; tabla++)
+		{
+			total = (por * tabla);
+			if (total > 9)
+			{
+				firstInt = (total / 10);
+				total = (total % 10);
+				_putchar(' ');
+				_putchar('0' + firstInt);
+				_putchar('0' + total);
+				_putchar(',');
+			}
+			else if (tabla == 0)
+			{
+				_putchar('0' + total);
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar('0' + total);
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
