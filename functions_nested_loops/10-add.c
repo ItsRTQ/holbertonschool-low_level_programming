88bbddaf720a781108f55aae5b6d5aa9774e856e
William <titanbabrisa@gmail.com>
2023-10-19 16:17:21 -0400

    first attempt

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adds to numbers
+ * @a: first parameter to be added
+ * @b: second parameter to be added
+ * Description - takes a, b as a parameter then adds them a+b
+ * Return: it returns an int value thats the total of a+b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
