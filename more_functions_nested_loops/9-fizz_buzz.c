c88c6bf5585215bf416e4857aefe8685ebf61f53
William <titanbabrisa@gmail.com>
2023-10-21 02:24:04 -0400

    attempt 4

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,19 +12,22 @@ int main(void)
 	{
 		if (counter % 3 == 0 && counter % 5 == 0)
 		{
-			printf(" FizzBuzz");
+			printf("FizzBuzz ");
 		}
 		else if (counter % 3 == 0)
 		{
-			printf(" Fizz");
+			printf("Fizz ");
 		}
 		else if (counter % 5 == 0)
 		{
-			printf(" Buzz");
+			printf("Buzz ");
 		}
 		else
 		{
-			printf(" %d", counter);
+			if (counter != 100)
+				printf("%d ", counter);
+			else
+				printf("%d", counter);
 		}
 	}
 	putchar('\n');
