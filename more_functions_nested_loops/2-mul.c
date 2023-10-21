f49407643c3b50a0fde3b9de4a2eb0dc7d377135
William <titanbabrisa@gmail.com>
2023-10-20 23:18:36 -0400

    first attemtp

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - it starts the program
+ * @a: recive a value
+ * @b: recive value 2
+ * Description - muliplies a and b
+ * Return: return the multiplication of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
