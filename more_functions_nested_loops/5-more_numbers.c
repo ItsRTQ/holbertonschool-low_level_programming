7fc0f6f066e703aee1bd942b0139fb54b5ca6538
William <titanbabrisa@gmail.com>
2023-10-20 23:46:41 -0400

    first attempt

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+* more_numbers - it starts the program
+* Description - counts 0-14 followed by a newline, 10 times
+* Return: void doesnt return
+*/
+void more_numbers(void)
+{
+	int a;
+	int b;
+	int firstInt;
+	int printVal;
+
+	for (b = 0; b <= 10; b++)
+	{
+		for (a = 0; a < 15; a++)
+		{
+			printVal = a;
+			if (a > 9)
+			{
+				firstInt = (a / 10);
+				_putchar('0' + firstInt);
+				printVal = (a % 10);
+			}
+			_putchar('0' + printVal);
+		}
+	_putchar('\n');
+	}
+}
