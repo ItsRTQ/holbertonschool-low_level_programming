47f8aeefac0c0efa887ab5331d1650d562f2fabe
William <titanbabrisa@gmail.com>
2023-10-23 17:10:54 -0400

    first attempt

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* reset_to_98 - it starts the program
+* @n: recive a value
+* Description - change value of n pointer to 98
+* Return: void, doesnt return
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
