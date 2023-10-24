1a7dac05e80f17cafd66bebd5904407b76c07e44
William <titanbabrisa@gmail.com>
2023-10-24 00:03:32 -0400

    first attempt

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_array - it starts the program
+* @a: recive a value
+* @n: recive value
+* Description - prints n amount of value of the given array
+* Return: void, doesnt return
+*/
+void print_array(int *a, int n)
+{
+	int num;
+
+	if (n <= 0)
+	{
+		printf("\n");
+		return;
+	}
+	for (num = 0; num < n; num++)
+	{
+		printf("%d", a[num]);
+		if (num < n -1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
