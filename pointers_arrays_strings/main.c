92f45b3f50337a2add2fddc5a6739dfc15d8dfc7
William <titanbabrisa@gmail.com>
2023-10-24 14:47:40 -0400

    first attempt

--- pointers_arrays_strings/main.c
+++ pointers_arrays_strings/main.c
@@ -1,33 +1,18 @@
+#include "main.h"
 #include <stdio.h>
 
-char *_strncpy(char *, char *, int);
-
 /**
- * main - check the code for Holberton School students.
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	char s1[98];
-	char *p;
-	int i;
+    char s1[] = "Hello";
+    char s2[] = "World!";
 
-	for (i = 0; i < 98 - 1; i++)
-		s1[i] = '-';
-	s1[i] = '\0';
-	printf("%s\n", s1);
-	p = _strncpy(s1, "Talk is cheap. Show me the code.\n", 64);
-	printf("%s\n", s1);
-	printf("%s\n", p);
-	for (i = 0; i < 98; i++)
-	{
-		if (i % 10)
-			printf(" ");
-		if (!(i % 10) && i)
-			printf("\n");
-		printf("0x%02x", s1[i]);
-	}
-	printf("\n");
-	return (0);
+    printf("%d\n", _strcmp(s1, s2));
+    printf("%d\n", _strcmp(s2, s1));
+    printf("%d\n", _strcmp(s1, s1));
+    return (0);
 }
