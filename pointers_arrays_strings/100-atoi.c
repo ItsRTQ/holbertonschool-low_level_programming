fb9ded40dc3215e4059e3b850faf167bded0873d
William <titanbabrisa@gmail.com>
2023-10-24 00:53:54 -0400

    first attempt

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* _atoi - it starts the program
+* @s: recive a value
+* Description - converts a string to numbers taking t account -+
+* Return: return the number converted to string
+*/
+int _atoi(char *s)
+{
+	int value = 0, a, negative = 1;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		if (s[a] >= '0' && s[a] <= '9')
+		{
+			if (s[a - 1] == '-')
+			{
+				negative = -1;
+			}
+			value = value * 10 + (s[a] - '0');
+		}
+	}
+	return (value * negative);
+}
