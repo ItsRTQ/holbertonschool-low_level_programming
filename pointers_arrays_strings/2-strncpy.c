2787313e3909ac823a18c78c3d7da266bc94b4f6
William <titanbabrisa@gmail.com>
2023-10-24 13:34:38 -0400

    4 attempt

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -15,16 +15,12 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 		for (a = 0; a < n; a++)
 		{
-			*dest = *src;
-			*dest++;
+			dest[a] = *src;
 		}
 	}
 	for (*dest; *dest < n; *dest++)
 	{
-		if (*src != '\0')
-		{
-			*dest = *src;
-		}
+		*dest = *src;
 	}
 	return (dest);
 }
