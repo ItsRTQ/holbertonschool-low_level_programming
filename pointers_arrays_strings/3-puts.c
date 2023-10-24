98724f5bd60db7e9b32adc5d7fdc3dc88a38d4f6
William <titanbabrisa@gmail.com>
2023-10-23 20:33:36 -0400

    first attempt

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _puts - it starts the program
+* @str: recive a value
+* Description - this function mimics the puts function
+* Return: void, doesnt return
+*/
+void _puts(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		_putchar(str[a]);
+	}
+}
