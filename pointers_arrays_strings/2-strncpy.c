9a90fd1a3584a02d13bae723b6446b1241154ae4
William <titanbabrisa@gmail.com>
2023-10-24 12:26:37 -0400

    1 attempt

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -5,12 +5,11 @@
 * @dest: recive a value
 * @src: second value
 * @n: third value
-* Description - this function is going to work exactly like the copystring function
+* Description - Works almost like the copystring function
 * Return: return a pointer dest after it is change
 */
 char *_strncpy(char *dest, char *src, int n)
 {
-	//int Size = _strlen(src);
 	int a;
 
 	for (a = 0; a < n; a++)
