ad3c4fa7f5e1b9834e0ef99963b4f27d0605e4bb
William <titanbabrisa@gmail.com>
2023-10-24 20:40:00 -0400

    first attempt

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* reverse_array - it starts the program
+* @a: recive a value
+* @n: recive value
+* Description - reverse an array of int values
+* Return: void, doesnt return
+*/
+void reverse_array(int *a, int n)
+{
+	int count;
+	char temp;
+
+	for (count = 0; count < n / 2; count++)
+	{
+		temp = a[count];
+		a[count] = a[n - count - 1];
+		a[n - count - 1] = temp;
+	}
+}
