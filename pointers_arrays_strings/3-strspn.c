42c9fa2084d76ae9a52f7e2dd984c3343cf5749f
William <titanbabrisa@gmail.com>
2023-10-26 22:04:41 -0400

    first attempt

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.c"
+/**
+* int _strspn - it starts the program
+* @s: recive a value
+* @accept: recive value
+* Description - gets the lenght of a prefix substring
+* Return: unsigned int with the initial segment of s
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int a, amount = 0, b;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		for(b = 0; accept[b] != '\0'; b++)
+		{
+			if(accept[b] == s[a])
+			{
+				amount++;
+				break;
+			}
+		}
+	}
+	return (amount);
+}
