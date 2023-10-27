227985109faa22ccfe3d0512744bd5b2eed31642
William <titanbabrisa@gmail.com>
2023-10-26 21:37:37 -0400

    2 attempt

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* *_strchr - it starts the program
+* @s: recive a value
+* @c: recive value
+* Description - checks if theres is a value in a memory block
+* Return: char pointer to the occurence of c in s or NULL if theres none
+*/
+char *_strchr(char *s, char c)
+{
+	char *ptr;
+	int a;
+
+	ptr = NULL;
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		if (s[a] == c)
+		{
+			ptr = &s[a];
+			return (ptr);
+		}
+	}
+	return (ptr);
+}
