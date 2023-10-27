24209371d9dce9c37b2c1b944a8826a043595870
William <titanbabrisa@gmail.com>
2023-10-26 20:26:51 -0400

    first commit

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* *_memset - it starts the program
+* @s: recive a value
+* @b: recive value
+* @n: recive value
+* Description - fills the first n bytes of the memory area pointed to by s
+* Return: char pointer with the desier value
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	int a;
+
+	for (a = 0; a < n; a++)
+	{
+		s[a] = b;
+	}
+	return (s);
+}
