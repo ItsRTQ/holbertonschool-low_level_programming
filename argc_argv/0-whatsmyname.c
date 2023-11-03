73216c15eb520ec41ea1b64ceabe03b6d647c6c7
William <titanbabrisa@gmail.com>
2023-11-03 19:37:17 -0400

    2 attempt

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -5,9 +5,13 @@
 * @argc: argument count
 * @argv: argument vector
 * Description - prints the name of the file
-* Return: void, doesnt return
+* Return: 0 if the function finish propertly
 */
-void main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
+	(void)argc;
+	(void)argv;
+
 	printf("%s\n", argv[0]);
+	return (0);
 }
