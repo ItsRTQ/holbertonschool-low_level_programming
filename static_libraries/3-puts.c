75a23625abce6609ca622aea531c2b135bdd14ef
William <titanbabrisa@gmail.com>
2023-11-03 02:28:05 -0400

    first attempt

---
+++ static_libraries/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "_putchar.c"
+/**
+* _puts - it starts the program
+* @str: recive a value
+* Description - this function mimics the puts function
+* Return: void, doesnt return
+*/
+void _puts(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
