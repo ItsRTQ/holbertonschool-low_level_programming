e9003787f7de2db2474282056a53efba777975eb
William <titanbabrisa@gmail.com>
2023-11-03 19:27:42 -0400

    first commit

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - it starts the program
+* Description - prints the name of the file
+* Return: void, doesnt return
+*/
+void main(int argc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+}
