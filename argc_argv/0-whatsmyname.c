8cead8772c0920ff2568917027286f563fe5c754
William <titanbabrisa@gmail.com>
2023-11-03 19:30:02 -0400

    first attempt

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,10 +2,12 @@
 #include <stdio.h>
 /**
 * main - it starts the program
+* @argc: argument count
+* @argv: argument vector
 * Description - prints the name of the file
 * Return: void, doesnt return
 */
-void main(int argc, char* argv[])
+void main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 }
