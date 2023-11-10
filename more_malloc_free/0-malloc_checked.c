3f76ed84619070a6f8bf96ba652d4c5c6bee5496
William <titanbabrisa@gmail.com>
2023-11-10 12:11:18 -0400

    first attempt

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* malloc_checked - it starts the program
+* @b: recive a value
+* Description - this function checks if malloc fails
+* Return: void, doesnt return
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *point = malloc(sizeof(int) * b);
+
+	if (point == NULL)
+	{
+		exit(98);
+	}
+	return (0);
+}
