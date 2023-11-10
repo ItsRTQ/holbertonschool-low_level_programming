5bf6936857b65eca7c2cd6ebfcefae409c31407f
William <titanbabrisa@gmail.com>
2023-11-10 16:08:57 -0400

    first attempt

---
+++ preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <string.h>
+#include <stdio.h>
+/**
+* main - it starts the program
+* Description - prints file name
+* Return: 0 if the function finish propertly
+*/
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
