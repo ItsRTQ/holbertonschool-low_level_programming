9faf2154602e144ddc2f8c519bce7e361294e0a6
William <titanbabrisa@gmail.com>
2023-11-13 17:11:21 -0400

    attempt 17

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -13,9 +13,12 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *Doggy;
-	char *name_copy = strdup(name);
-	char *owner_copy = strdup(owner);
+	char *name_copy, *owner_copy;
 
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	name_copy = strdup(name);
+	owner_copy = strdup(owner);
 	if (name_copy == NULL || owner_copy == NULL)
 	{
 		free(name_copy);
