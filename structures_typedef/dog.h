80a91d4d8e42a39b7218b8214e1b2f1d6c72143d
William <titanbabrisa@gmail.com>
2023-11-13 12:08:05 -0400

    first commit

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+* struct dog - dog basic DATA
+* @name: First member
+* @age: Second member
+* @owner: third member
+*
+* Description: this struct will have the Data of a dog
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
