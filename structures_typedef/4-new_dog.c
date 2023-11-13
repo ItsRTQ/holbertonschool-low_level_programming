f91b71b4d1ebb5e590e123a42cccb415f6b2b277
William <titanbabrisa@gmail.com>
2023-11-13 16:52:46 -0400

    attempt 15

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -13,23 +13,25 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *Doggy;
+	char *name_copy = strdup(name);
+	char *owner_copy = strdup(owner);
 
+	if (name_copy == NULL || owner_copy == NULL)
+	{
+		free(name_copy);
+		free(owner_copy);
+		return (NULL);
+	}
 	Doggy = malloc(sizeof(dog_t));
 	if (Doggy == NULL)
-		return (NULL);
-	Doggy->name = strdup(name);
-	if (Doggy->name == NULL)
 	{
+		free(name_copy);
+		free(owner_copy);
 		free(Doggy);
 		return (NULL);
 	}
-	Doggy->owner = strdup(owner);
-	if (Doggy->owner == NULL)
-	{
-		free(Doggy->name);
-		free(Doggy);
-		return (NULL);
-	}
+	Doggy->name = name_copy;
+	Doggy->owner = owner_copy;
 	Doggy->age = age;
 	return (Doggy);
 }
