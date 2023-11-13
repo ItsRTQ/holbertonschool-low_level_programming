74d60d46c658084568d0ec414bf32a0f376de0ae
William <titanbabrisa@gmail.com>
2023-11-13 14:03:58 -0400

    first attempt

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+* new_dog - it starts the program
+* @name: recive a value
+* @age: value
+* @owner: value
+* Description - creates a new dog struct
+* Return: a structure dog struct, or null if fail
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *Doggy;
+
+	Doggy = malloc(sizeof(dog_t));
+	if (Doggy == NULL)
+	{
+		return (NULL);
+	}
+	Doggy->name = strdup(name);
+	Doggy->age = age;
+	Doggy->owner = strdup(owner);
+	return (Doggy);
+}
