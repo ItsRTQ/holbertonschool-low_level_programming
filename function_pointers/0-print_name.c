596ea507f4679ebd9f391bb047c2536ae1afc6ba
William <titanbabrisa@gmail.com>
2023-11-14 16:02:51 -0400

    first attempt

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+* print_name - it starts the program
+* @name: name to print
+* @f: is a function pointer, will recive the function address
+* Description - does callback of the recive function
+* Return: void, doesnt return
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
