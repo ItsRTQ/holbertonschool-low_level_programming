115b0ba3be1193aa11f6165fb4a1c3f364e40e77
William <titanbabrisa@gmail.com>
2023-11-14 19:53:23 -0400

    first attempt

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,5 +1,6 @@
-#include "calc.h"
+#include "3-calc.h"
 #include <stdlib.h>
+#include <string.h>
 /**
 * get_op_func - compares a value
 * @s: operator to use
@@ -20,7 +21,7 @@ int (*get_op_func(char *s))(int, int)
 
 	while (ops[i].op != NULL)
 	{
-		if (ops[i].op == s)
+		if (strcmp(ops[i].op, s) == 0)
 		{
 			return (ops[i].f);
 		}
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,6 +1,7 @@
-#include "calc.h"
+#include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
 * main - it starts the program
 * @argv: argument vector
@@ -10,5 +11,29 @@
 */
 int main(int argc, char *argv[])
 {
+	int num1, num2;
+	int (*calc)(int, int);
+	char *operator;
+
+	if (argc != 4)
+	{
+		printf("ERROR\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operator = argv[2];
+	if ((num1 == 0 || num2 == 0) && (strcmp(operator, "/") == 0 || strcmp(operator, "%") == 0))
+	{
+		printf("ERROR\n");
+		exit(100);
+	}
+	calc = get_op_func(operator);
+	if (calc == NULL)
+	{
+		printf("ERROR\n");
+		exit(99);
+	}
+	printf("%d\n", calc(num1, num2));
 	return (0);
 }
