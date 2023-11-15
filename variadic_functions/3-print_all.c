238f16d77c7d7817cd6b88416ed944f4bd89005c
William <titanbabrisa@gmail.com>
2023-11-15 03:43:52 -0400

    attempt 2

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,6 +2,45 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define NEW_LINE printf("\n")
+/**
+ * commaChecker - checks if comma are needed
+ * @word: needed parameter
+ * @n: index to check
+ *
+ * Return: the needed string
+ */
+char *commaChecker(const char * const word, int n)
+{
+	char *comma = ", ";
+	char *empty = "";
+
+	switch (word[(n + 1)])
+	{
+		case '\0':
+			return (empty);
+		default:
+			return (comma);
+	}
+}
+/**
+ * nulVerify - verify if the src is null
+ * @src: string to check
+ *
+ * Return: nil in case of null or src otherwise
+ */
+char *nulVerify(char *src)
+{
+	char *new;
+
+	if (src == NULL)
+	{
+		new = strdup("(nil)");
+		return (new);
+	}
+	return (src);
+}
 /**
 * print_all - prints anything, recive in the arguments
 * @format: type of the arguments
@@ -11,43 +50,37 @@
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	int i = 0, fakeBool;
+	int i = 0;
+	char *str, *toFree;
 
-	if (format == NULL)
-		return;
-	va_start(args, format);
-	while (format[i] != '\0')
+	if (format != NULL)
 	{
-		fakeBool = 0;
-		switch (format[i])
+		va_start(args, format);
+		while (format[i] != '\0')
 		{
-			case 'c':
-				{
-					printf("%c", (char)va_arg(args, int));
-					break;
-				}
-			case 'i':
-				{
-					printf("%d", va_arg(args, int));
-					break;
-				}
-			case 'f':
-				{
-					printf("%f", va_arg(args, double));
-					break;
-				}
-			case 's':
-				{
-					printf("%s", va_arg(args, char*));
-					break;
-				}
-			default:
-				fakeBool = 1;
+			switch (format[i])
+			{
+				case 'c':
+						printf("%c%s", (char)va_arg(args, int), commaChecker(format, i));
+						break;
+				case 'i':
+						printf("%d%s", va_arg(args, int), commaChecker(format, i));
+						break;
+				case 'f':
+						printf("%f%s", va_arg(args, double), commaChecker(format, i));
+						break;
+				case 's':
+						str = va_arg(args, char*);
+						toFree = nulVerify(str);
+						printf("%s%s", toFree, commaChecker(format, i));
+						free(toFree);
+						break;
+				default:
+						break;
+			}
+			i++;
 		}
-		i++;
-		if (format[i] != '\0' && fakeBool != 1)
-			printf(", ");
+		va_end(args);
 	}
-	va_end(args);
-	printf("\n");
+	NEW_LINE;
 }
