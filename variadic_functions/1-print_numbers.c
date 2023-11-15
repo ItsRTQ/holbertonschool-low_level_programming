7dff90ec5d8a478f486326b4926158be6b72c893
William <titanbabrisa@gmail.com>
2023-11-14 23:32:47 -0400

    attempt 2

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -15,13 +15,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i = 0;
 	int num;
 
-	if (separator == NULL)
+	if (separator == NULL || n == 0)
 	{
 		i = n;
-	}
-	if (n == 0)
-	{
-		return;
 	}
 	va_start(args, n);
 	for (; i < n; i++)
