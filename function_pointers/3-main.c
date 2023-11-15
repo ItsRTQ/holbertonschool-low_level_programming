1d61916022d4acb8eee7c5ffe78bfb95159374f5
William <titanbabrisa@gmail.com>
2023-11-14 20:15:23 -0400

    attempt 2

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define ZRO(x, y) ((x) == 0 || (y) == 0)
 /**
 * main - it starts the program
 * @argv: argument vector
@@ -11,29 +12,29 @@
 */
 int main(int argc, char *argv[])
 {
-	int num1, num2;
+	int n1, n2;
 	int (*calc)(int, int);
 	char *operator;
 
 	if (argc != 4)
 	{
-		printf("ERROR\n");
+		printf("Error\n");
 		exit(98);
 	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[3]);
 	operator = argv[2];
-	if ((num1 == 0 || num2 == 0) && (strcmp(operator, "/") == 0 || strcmp(operator, "%") == 0))
+	if (ZRO(n1, n2) && (strcmp(operator, "/") == 0 || strcmp(operator, "%") == 0))
 	{
-		printf("ERROR\n");
+		printf("Error\n");
 		exit(100);
 	}
 	calc = get_op_func(operator);
 	if (calc == NULL)
 	{
-		printf("ERROR\n");
+		printf("Error\n");
 		exit(99);
 	}
-	printf("%d\n", calc(num1, num2));
+	printf("%d\n", calc(n1, n2));
 	return (0);
 }
