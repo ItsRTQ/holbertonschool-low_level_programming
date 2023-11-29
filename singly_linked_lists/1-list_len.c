4fd190924cc3e3d82e573ad8b8316d7e8ed4e8de
William <titanbabrisa@gmail.com>
2023-11-29 04:18:09 -0400

    assigment 1 list len

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,1 +1,18 @@
+#include "lists.h"
+/**
+* list_len - checks how many elements are in a link list
+* @h: head of link list
+*
+* Return: size_t that the amount of elements
+*/
+size_t list_len(const list_t *h)
+{
+	size_t amount = 0;
+	const list_t *currentNode = h;
 
+	for (; currentNode != NULL; currentNode = currentNode->next)
+	{
+		amount++;
+	}
+	return (amount);
+}
