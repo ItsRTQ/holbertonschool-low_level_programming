1a9a86895567197646ba4762b6b4682f8d73be89
William <titanbabrisa@gmail.com>
2023-11-29 05:34:48 -0400

    real attempt of add node to tail

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+/**
+* add_node_end - adds a new node to the tail of the list
+* @head: recive head node
+* @str: to add in this new node
+*
+* Return: a pointer to the head of the list
+*/
+list_t *add_node_end(list_t **head, const char *str)
+{
+	unsigned int size = 0;
+	list_t *newNode;
+
+	newNode = (list_t *)malloc(sizeof(list_t));
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
+	if (str != NULL)
+	{
+		newNode->str = strdup(str);
+		if (newNode->str == NULL)
+		{
+			free(newNode);
+			return (NULL);
+		}
+		size = strlen(newNode->str);
+	}
+	else
+	{
+		newNode->str = "";
+	}
+	newNode->len = size;
+	newNode->next = NULL;
+	if (*head == NULL)
+	{
+		*head = newNode;
+		return (*head);
+	}
+
+	list_t *currentNode = *head;
+
+	for (; currentNode->next != NULL; currentNode = currentNode->next)
+	{
+	}
+	currentNode->next = newNode;
+	return (*head);
+}
