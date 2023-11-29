8ea35ce8bdd0414140b15a79336a677ae2bb11f5
William <titanbabrisa@gmail.com>
2023-11-29 03:43:38 -0400

    first commit

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib>
+#include <stdio.h>
+/**
+* print_list - prints a link list value
+* @h: head of the list
+*
+* Return: size_t that the amount of nodes
+*/
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+	list_t *currentNode = &h;
+
+	for (currentNode = &h; currentNode != NULL; currentNode = currentNode->next)
+	{
+		printf("[%u] ", (currentNode->str == NULL) ? 0 : currentNode->len)
+		printf("%s\n", (currentNode->str == NULL) ? "(nil)" : currentNode->str);
+		i++;
+	}
+	return (i);
+}
