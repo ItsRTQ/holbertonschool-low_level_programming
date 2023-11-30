ea67f65e9bea855bfd99e8225a06f1052925e28d
William <titanbabrisa@gmail.com>
2023-11-30 16:07:23 -0400

    task 1

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_binary - prints recive value in binary
+* @n: recive a value
+*
+* Return: void, doesnt return
+*/
+void print_binary(unsigned long int n)
+{
+	int bits;
+
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+	bits = (n & 1);
+	printf("%c", (bits + '0'));
+}
