66da61b21479b72b6c6d7907931077b353cb302a
William <titanbabrisa@gmail.com>
2023-12-08 21:30:48 -0400

    task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -12,9 +12,12 @@ int create_file(const char *filename, char *text_content)
 	ssize_t checks;
 	size_t size;
 
-	if (text_content == NULL || filename == NULL)
+	if (filename == NULL)
 		return (-1);
-	size = strlen(text_content);
+	if (text_content != NULL)
+		size = strlen(text_content);
+	else
+		size = 0;
 	file_des = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (file_des == -1)
 		return (-1);
