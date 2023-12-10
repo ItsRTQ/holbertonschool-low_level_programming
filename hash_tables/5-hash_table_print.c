7f211f474c49dc311079236039e426456b615578
William <titanbabrisa@gmail.com>
2023-12-10 01:52:13 -0400

    task 5

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+/**
+* hash_table_print - prints hash table elements
+* @ht: hash table to print
+*
+* Return: void, doesnt return
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *current;
+	int flag = 0;
+
+	printf("{");
+	if (ht != NULL)
+	{
+		for (i = 0; ht && i < ht->size; i++)
+		{
+			if (ht->array[i] != NULL)
+			{
+				current = ht->array[i];
+				while (current)
+				{
+					printf("%s'%s': '%s'", flag ? ", " : "", current->key, current->value);
+					current = current->next;
+					flag = 1;
+				}
+			}
+		}
+	}
+	printf("}\n");
+}
